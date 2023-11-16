72f9c8f7fd4857d32b84ba56117af2d76682436a
S4monts <147527466+S4monts@users.noreply.github.com>
2023-11-16 13:35:45 -0500

    Add files via upload

---
+++ V.Final.MP MUSIC/src/mp/music/HISTORIALController.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mp.music;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Manuel Pacheco
+ */
+public class HISTORIALController implements Initializable {
+    
+    REGISTRO list = new REGISTRO();
+    
+    @FXML
+    private TableView<instrumento> table;
+    
+    @FXML
+    private TableColumn<instrumento, String> nom;
+    
+    @FXML
+    private TableColumn<instrumento, String> desc;
+    
+    @FXML
+    private TableColumn<instrumento, String> precio;
+    
+    public static ObservableList<instrumento> historia = FXCollections.observableArrayList();
+    
+    @FXML
+    private Button btnRegresar;
+    
+    @FXML
+    private void actionEvent(ActionEvent e){
+        Object evt = e.getSource();
+        if(evt.equals(btnRegresar)){
+            LoadStage("PRINCIPAL.fxml", e);
+        }
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Stack<instrumento> p;
+        p = carrito.pila;
+        nom.setCellValueFactory(new PropertyValueFactory<instrumento, String>("nom"));
+        desc.setCellValueFactory(new PropertyValueFactory<instrumento, String>("desc"));
+        precio.setCellValueFactory(new PropertyValueFactory<instrumento, String>("precio"));
+        table.setItems(historia);
+        // TODO
+    }    
+    private void LoadStage(String url, Event event){
+        try {
+            Object eventSource = event.getSource();
+            Node sourceAsNode = (Node) eventSource;
+            Scene oldScene = sourceAsNode.getScene();
+            Window window = oldScene.getWindow();
+            Stage stage = (Stage) window;
+            stage.hide();
+            
+            Parent root = FXMLLoader.load(getClass().getResource(url));
+            Scene scene = new Scene(root);
+            Stage newStage = new Stage();
+            newStage.setScene(scene);
+            newStage.show();
+            
+            newStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    Platform.exit();
+                }        
+            });
+        }catch ( IOException ex){
+            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE,null, ex);
+        }
+    } 
+}
---
+++ V.Final.MP MUSIC/src/mp/music/MenuController.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mp.music;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Manuel Pacheco 
+ */
+public class MenuController implements Initializable {
+    
+    REGISTRO list = new REGISTRO();
+    
+    public static carrito pila = new carrito();
+    
+    
+    public static String nom1, nom2, nom3,lima1,lima2,lima3,pre1,pre2,pre3,desc1,desc2,desc3;
+    
+    @FXML
+    private Label lbI1, lbI2, lbI3,lbprecio1,lbprecio2,lbprecio3;
+    
+    @FXML
+    private ImageView ima1,ima2,ima3;
+    
+    @FXML
+    private Button btnRegresar,btninfo1,btninfo2,btninfo3;
+    
+    @FXML
+    private void actionEvent (ActionEvent e){
+        Object evt = e.getSource();
+        if(evt.equals(btnRegresar)){
+            LoadStage("PRINCIPAL.fxml", e);
+        }
+        if(evt.equals(btninfo1)){
+            instrumento instru1 = new instrumento(nom1, desc1, pre1, INICIOController.UsuarioActual);
+            pila.push(instru1.nom,instru1.desc,instru1.precio,instru1.comprador);
+            LISTADOController.instrumentos.add(instru1);
+        }
+        if(evt.equals(btninfo2)){
+            instrumento instru2 = new instrumento(nom2, desc2, pre2, INICIOController.UsuarioActual);
+            pila.push(instru2.nom,instru2.desc,instru2.precio,instru2.comprador);
+            LISTADOController.instrumentos.add(instru2);
+        }
+        if(evt.equals(btninfo3)){
+            instrumento instru3 = new instrumento(nom3, desc3, pre3, INICIOController.UsuarioActual);
+            pila.push(instru3.nom,instru3.desc,instru3.precio,instru3.comprador);
+            LISTADOController.instrumentos.add(instru3);
+        }
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Stack<instrumento> p;
+        p = carrito.pila;
+        lbI1.setText(nom1);
+        lbI2.setText(nom2);
+        lbI3.setText(nom3);
+        lbprecio1.setText(pre1);
+        lbprecio2.setText(pre2);
+        lbprecio3.setText(pre3);
+        Image show1 = new Image(lima1);
+        ima1.setImage(show1);
+        Image show2= new Image(lima2);
+        ima2.setImage(show2);
+        Image show3 = new Image(lima3);
+        ima3.setImage(show3);
+        // TODO
+    }    
+    private void LoadStage(String url, Event event){
+        try {
+            Object eventSource = event.getSource();
+            Node sourceAsNode = (Node) eventSource;
+            Scene oldScene = sourceAsNode.getScene();
+            Window window = oldScene.getWindow();
+            Stage stage = (Stage) window;
+            stage.hide();
+            
+            Parent root = FXMLLoader.load(getClass().getResource(url));
+            Scene scene = new Scene(root);
+            Stage newStage = new Stage();
+            newStage.setScene(scene);
+            newStage.show();
+            
+            newStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    Platform.exit();
+                }        
+            });
+        }catch ( IOException ex){
+            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE,null, ex);
+        }
+    }
+}
